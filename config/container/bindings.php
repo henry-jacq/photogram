5cfd555bb56d2ed04395c73558083a530a0bceab
henry-jacq <henryeditz267@gmail.com>
2024-07-16 21:16:20 +0530

    feat: Doctrine ORM added

--- config/container/bindings.php
+++ config/container/bindings.php
@@ -2,15 +2,14 @@
 
 use Slim\App;
 use App\Core\View;
-use App\Model\Post;
 use App\Model\User;
 use App\Core\Config;
-use App\Model\Image;
 use App\Core\Request;
 use App\Core\Session;
-use App\Core\Database;
 use function DI\create;
 use Slim\Factory\AppFactory;
+use Doctrine\ORM\ORMSetup;
+use Doctrine\ORM\EntityManager;
 use App\Interfaces\SessionInterface;
 use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseFactoryInterface;
@@ -30,6 +29,13 @@ return [
 
         return $app;
     },
+    EntityManager::class => fn (Config $config) => EntityManager::create(
+        $config->get('doctrine.connection'),
+        ORMSetup::createAttributeMetadataConfiguration(
+            $config->get('doctrine.entity_dir'),
+            $config->get('doctrine.dev_mode')
+        )
+    ),
     Config::class => create(Config::class)->constructor(
         require CONFIG_PATH . '/app.php'
     ),
@@ -40,40 +46,13 @@ return [
         return new Session($container->get(Config::class));
     },
     ResponseFactoryInterface::class => fn(App $app) => $app->getResponseFactory(),
-    Database::class => function (ContainerInterface $container) {
-        $config = $container->get(Config::class)->get('db');
-        $pdo = new \PDO(
-            "{$config['driver']}:host={$config['host']};port={$config['port']};dbname={$config['dbname']}",
-            $config['user'],
-            $config['pass'],
-            [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]
-        );
-        return Database::getConnection($pdo);
-    },
     Request::class => function(ContainerInterface $container) {
         return new Request($container->get(SessionInterface::class));
     },
-    Image::class => function () {
-        if (extension_loaded('gd')) {
-            return new Image();
-        }
-        throw new Exception('GD Extension is not loaded.');
-    },
     User::class => function(ContainerInterface $container){
-        return new User(
-            $container->get(Image::class),
-            $container->get(Database::class),
-            $container->get(SessionInterface::class),
-        );
+        return new User();
     },
     ZipArchive::class => function () {
         return new ZipArchive();
-    },
-    Post::class => function (ContainerInterface $container) {
-        return new Post(
-            $container->get(Image::class),
-            $container->get(Database::class),
-            $container->get(ZipArchive::class)
-        );
-    },
+    }
 ];
