9c2266c6dbd289599df6a4aeac39bc6bff9a3142
henry-jacq <henryeditz267@gmail.com>
2024-07-20 22:54:02 +0530

    change: new styles to discover page

--- views/templates/user/discover.php
+++ views/templates/user/discover.php
@@ -1,6 +1,6 @@
 <div class="discover-section pt-4 mb-5 px-2">
     <div class="d-flex align-items-center flex-column justify-content-center">
-        <div class="col-12 col-md-7 col-lg-6 mt-3 mb-5">
+        <div class="col-12 col-md-10 col-lg-6 mt-3 mb-5">
             <form class="text-body position-relative ms-auto my-md-0 my-sm-3" role="search">
                 <div class="input-group mb-3">
                     <input class="form-control search-input shadow" type="search" name="search" placeholder="Search" aria-label="Search" aria-describedby="search-blogs">
