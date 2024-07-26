bea8bba26049ebe71093d1859f241937049a2872
henry-jacq <henryeditz267@gmail.com>
2024-07-27 00:39:15 +0530

    feat: Remove user home view mode functionality from follow API

--- routes/api/users/follow.php
+++ routes/api/users/follow.php
@@ -14,18 +14,6 @@ ${basename(__FILE__, '.php')} = function () {
                 'message' => $result
             ], 200);
         }
-
-        // To change user home view mode
-        if ($this->paramsExists(['view'])) {
-            $id = $this->getUserId();
-            $mode = $this->data['view'] ?? "grid";
-
-            $result = $this->user->updatePreference($id, "view", $mode);
-
-            return $this->response([
-                'message' => $result
-            ], 200);
-        }
     }
     return $this->response([
         'message' => 'Bad Request'
