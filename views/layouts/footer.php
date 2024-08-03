3c4c20df41d1f4f58ab34332695b2dff5e827239
henry-jacq <henryeditz267@gmail.com>
2024-08-03 20:46:47 +0530

    Update footer layout and content

--- views/layouts/footer.php
+++ views/layouts/footer.php
@@ -1,8 +1,52 @@
-<div class="container p-3 mb-3 mt-auto">
-    <footer class="py-5 bg-body-tertiary rounded-5 border shadow-lg">
-        <div class="text-center text-body-secondary">
-            <p class="fs-5">©Copyrights <?= date('Y') . '-' . date('Y') + 1 ?>. All rights reserved.</p>
-            <span class="fs-5">Made with ❤ by <a href="https://github.com/henry-jacq">Henry</a>.</span>
+<div class="container px-md-5">
+    <footer class="px-3">
+        <div class="row">
+            <div class="col-6 col-md-3 mb-3">
+                <h5>Photogram</h5>
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-2"><a href="/home" class="nav-link p-0 text-body-secondary">Home</a></li>
+                    <li class="nav-item mb-2"><a href="/discover" class="nav-link p-0 text-body-secondary">Discover</a></li>
+                    <li class="nav-item mb-2"><a href="/pro/plans" class="nav-link p-0 text-body-secondary">Pricing</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>
+                </ul>
+            </div>
+
+            <div class="col-6 col-md-3 mb-3">
+                <h5>Company</h5>
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-2"><a href="/about-us" class="nav-link p-0 text-body-secondary">About</a></li>
+                    <li class="nav-item mb-2"><a href="/contact-us" class="nav-link p-0 text-body-secondary">Contact us</a></li>
+                    <li class="nav-item mb-2"><a href="/privacy-policy" class="nav-link p-0 text-body-secondary">Privacy Policy</a></li>
+                    <li class="nav-item mb-2"><a href="/terms-and-conditions" class="nav-link p-0 text-body-secondary">Terms and Conditions</a></li>
+                </ul>
+            </div>
+
+            <div class="col-md-5 offset-md-1 mb-3">
+                <form>
+                    <h5>Subscribe to our newsletter</h5>
+                    <p>Monthly digest of what's new and exciting from us.</p>
+                    <div class="d-flex flex-column flex-sm-row w-100 gap-2">
+                        <label for="newsletter1" class="visually-hidden">Email address</label>
+                        <input id="newsletter1" type="text" class="form-control" placeholder="Email address">
+                        <button class="btn btn-primary" type="button">Subscribe</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+        <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
+            <p>© 2024 Photogram, Inc. All rights reserved.</p>
+            <ul class="list-unstyled d-flex">
+                <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi" width="24" height="24">
+                            <use xlink:href="#twitter"></use>
+                        </svg></a></li>
+                <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi" width="24" height="24">
+                            <use xlink:href="#instagram"></use>
+                        </svg></a></li>
+                <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi" width="24" height="24">
+                            <use xlink:href="#facebook"></use>
+                        </svg></a></li>
+            </ul>
         </div>
     </footer>
 </div>
