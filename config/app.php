0591e49c85787ac55879153404c3bf3eb1dc71f7
henry-jacq <henryeditz267@gmail.com>
2024-08-23 08:57:04 +0000

    Update database configuration to use environment variables

--- config/app.php
+++ config/app.php
@@ -29,11 +29,11 @@ return [
         'entity_dir' => [APP_PATH . '/Entity'],
         'connection' => [
             'driver' => $_ENV['DB_DRIVER'] ?? 'pdo_mysql',
-            'host' => $_ENV['DB_HOST'] ?? 'localhost',
+            'host' => $_ENV['DB_HOST'],
             'port' => $_ENV['DB_PORT'] ?? 3306,
-            'dbname' => $_ENV['DB_NAME'] ?? 'photogram_db',
-            'user' => $_ENV['DB_USER'] ?? 'henry',
-            'password' => $_ENV['DB_PASS'] ?? 'lamp@unix098',
+            'dbname' => $_ENV['DB_NAME'],
+            'user' => $_ENV['DB_USER'],
+            'password' => $_ENV['DB_PASS'],
         ]
     ],
     'mail' => [
